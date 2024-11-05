89a6713a630cd09277fc5e7ccff533ccae9059cc
Prasad Joshi <ppjoshi2003@gmail.com>
2024-11-05 14:19:22 +0530

    middleware example

--- my-app_hono/src/index.ts
+++ my-app_hono/src/index.ts
@@ -1,9 +1,34 @@
-import { Hono } from 'hono'
+import { Hono, Next } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+
+// simple auth middleware
+async function authMiddlware(c: any, Next: any) {
+  if(c.req.header("Authorization")) {
+    // do authentication
+
+    await Next();
+  }else {
+    return c.text("you do not have access")
+  }
+}
+// -> you can use any one either app.use() or pass it in the request body
+// app.use(authMiddlware)
+
+app.post('/', authMiddlware, async (c) => {
+ 
+  const body = await c.req.json()
+
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query());
+
   return c.text('Hello Hono!')
 })
+
+
+
+
 
 export default app
